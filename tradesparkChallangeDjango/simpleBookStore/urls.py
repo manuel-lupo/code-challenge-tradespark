a7f0b58bc15a734ec1af3239454d948144d9937b
Manuel Lupo <mlupo@alumnos.exa.unicen.edu.ar>
2023-12-30 22:18:55 -0300

    Django: agregado path remove-category para enrutar la eliminacion de una categoria para un libro

--- tradesparkChallangeDjango/simpleBookStore/urls.py
+++ tradesparkChallangeDjango/simpleBookStore/urls.py
@@ -8,8 +8,9 @@ router.register(r'authors', AuthorViewSet)
 router.register(r'categories', CategoryViewSet)
 router.register(r'books', BookViewSet)
 
+book_router = DefaultRouter()
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('remove-category/', BookViewSet.as_view({'put': 'remove_category_from_book'}), name='remove-category-from-book')
 ]
-
-
