fb0e0fcf593af6f42b4f11cffb877e36d586a4b0
Manuel Lupo <mlupo@alumnos.exa.unicen.edu.ar>
2023-12-30 22:47:48 -0300

    Django: Comentarios en el proyecto

--- tradesparkChallangeDjango/simpleBookStore/urls.py
+++ tradesparkChallangeDjango/simpleBookStore/urls.py
@@ -12,5 +12,6 @@ book_router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
+    #Elijo hacer la ruta mediante un metodo put ya que aunque se esta eliminando una entrada de la tabla intermedia, se esta modificando el libro y la categoria
     path('remove-category/', BookViewSet.as_view({'put': 'remove_category_from_book'}), name='remove-category-from-book')
 ]
